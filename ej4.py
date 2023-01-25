4d6f0a76f5e4e238bd318a47aaa2e2c001c6bbbf
Serjo <savenkoserhii@knu.ua>
2023-01-25 17:12:45 +0200

    ej4 additional from leetcode

--- ej4.py
+++ ej4.py
@@ -1,4 +1,6 @@
 # Largest palindrome product
+from pip._vendor.pyparsing import Optional
+
 
 def pal_check(num):
     numsFromPalindromNumbArr = [int(a) for a in str(num)]  # we make list from our number in order to evaluate numbers
@@ -17,3 +19,24 @@ for i in range(900, 1000):
         if pal_check(i * a):
             palindromeArr.append(i * a)
 print(max(palindromeArr))
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:  # slow
+        NuElLi = []
+        while head is not None:
+            NuElLi.append(head.val)
+            head = head.next
+        equality = True
+        while len(NuElLi) > 1 and equality:
+            starting = NuElLi.pop(0)
+            ending = NuElLi.pop(len(NuElLi) - 1)
+            if starting != ending:
+                equality = False
+        return equality
