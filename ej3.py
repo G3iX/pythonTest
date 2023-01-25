1dc77c78edcfab7efdc1d18eb916fa2a23c2815b
Serjo <savenkoserhii@knu.ua>
2023-01-25 17:15:21 +0200

    memorry error

--- ej3.py
+++ ej3.py
@@ -1,11 +1,47 @@
 # Largest prime factor
+
+
+def eratosthenes_sieve(N):  # error
+    a = 2
+    if N > 600851475143:
+        a = 600851475143 - 851475143
+    else:
+        a = 2
+    all_nums_arr = list(range(a, N + 1))
+    all_nums_arr[1] = 0
+    # prime_nums_arr = []
+    # first_prime = 2
+    for first_prime in all_nums_arr:
+        if all_nums_arr[first_prime] != 0:
+            # prime_nums_arr.append(all_nums_arr[first_prime])
+            for prime_num in range(first_prime, N + 1, first_prime):
+                all_nums_arr[prime_num - 2] = 0
+        first_prime += 1
+    prime_nums_arr = sorted(list(set(all_nums_arr)))
+    prime_nums_arr.pop(0)
+    print(prime_nums_arr)
+
+
 # 600851475143
 primeNumsArr = [2]
-for Num in range(3, 15):
+# print(end="[")
+for Num in range(3, 6000, 2):
+    isPrime = True
+    if Num > 10:
+        if Num % 10 == 5:
+            continue
     for primeNum in primeNumsArr:
-        # print(Num, " % ", primeNum, "==", Num % primeNum) # inf. :(
-        if Num % primeNum != 0:
-            primeNumsArr.append(primeNum)
-        else:
+        if primeNum * primeNum - 1 < Num:
+            continue
+        if Num % primeNum == 0:
+            isPrime = False
             break
-print(primeNumsArr)
+    if isPrime:
+        primeNumsArr.append(Num)
+        # print(".", end="")
+# print("]")
+for i in range(6):
+    primeNumsArr.pop(0)
+print(primeNumsArr)  # max(
+
+eratosthenes_sieve(600851475143)
