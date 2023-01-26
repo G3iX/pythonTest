37ddb1430383425d346edee20fc1873844d95306
Serjo <savenkoserhii@knu.ua>
2023-01-26 12:27:35 +0200

    #5 task - done

--- ej5.py
+++ ej5.py
@@ -1,2 +1,19 @@
 # Smallest multiple
 # 2520
+def type2():
+    maxRange = 20
+    lst = range(maxRange, 0, -1)
+    count = 1
+    sum = maxRange
+
+    lstCount = lst[count]
+    while lstCount != 1:
+        tmp = sum
+        while sum % lstCount != 0:
+            sum += tmp
+        count += 1
+        lstCount = lst[count]
+    print(sum)
+
+
+type2()
