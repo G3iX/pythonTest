d44901f8c515f9d631b48bad860154a8eb62a191
Serjo <savenkoserhii@knu.ua>
2023-01-26 12:42:35 +0200

    #6 task - done

--- ej6.py
+++ ej6.py
@@ -1,50 +1,16 @@
-# leetcode 13 - Roman to Integer
+# Sum square difference
+# - N^2 + N^3 (N in range(101))
+
+def hundred():
+    sum2 = 0
+    sum3 = sum2
+    for i in range(1, 101):
+        sum2 += i
+        sum3 += i * i
+        # print(i, "+ itself = ", sum2, ";")
+        # print(i, "^2= ", sum3, ";")
+    sum2 = sum2*sum2
+    return sum2 - sum3
 
 
-def rToIn(s):
-    RtoID = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    iNum = 0
-    RnL = []
-    InL= []
-    for i in s:
-        RnL.append(i)
-    for i in RnL:
-        InL.append(RtoID[i])
-    # iNum += InL[0]
-    for i in range(0, len(InL)-1):
-        if InL[i] >= InL[i+1]:
-            iNum += InL[i]
-            # print(iNum)
-        else:
-            iNum -= InL[i]
-            # print("InL[i] - InL[i - 1] = ", InL[i], "-", InL[i - 1])
-        #
-    iNum += InL[len(InL) - 1]
-    print("-------")
-    print(iNum)
-
-
-rToIn("XLIX")
-
-
-def roicat(s):
-    rtoi = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    inum = 0
-    temp = []
-    for i in s:
-        if len(s) > 1:
-            temp.append(rtoi[i])
-        else:
-            inum = rtoi[i]
-            break
-    for i in range(1, len(temp)):
-        inum += temp[0]
-        if temp[i - 1] < temp[i]:
-            inum -= temp[i]
-        else:
-            inum += temp[i]
-    if inum < 0:
-        inum -= inum * 2
-    print(inum)
-
-# roicat("LVIII")
+print(hundred())
